20c11252fcb4f48e04a8677fc89be0d7d9feddaa
tal shachar <tal.shachar@buzzfeed.com>
2018-03-01 20:00:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,6 @@
 def starts_with_a_vowel?(word)
-if word.match(/\b[aeiouAEIOU]/)
-  return true
-else false
+word.match(/\b[aeiouAEIOU]/)
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
