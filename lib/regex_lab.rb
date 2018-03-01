fe8443001995545cf5cc74cb4253b4b5086e0c2e
tal shachar <tal.shachar@buzzfeed.com>
2018-03-01 20:01:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,8 @@
 def starts_with_a_vowel?(word)
-word.match(/\b[aeiouAEIOU]/)
-  
+if word.match(/\b[aeiouAEIOU]/) == true
+  true
+else
+  false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
